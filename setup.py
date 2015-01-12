cc3dff0b8bb28320a930fb10ea889a9e37efb7b7
minillinim <mike@mikeimelfort.com>
2015-01-12 14:21:42 +1000

    Removed soname and ability to make shared library due to issues with MAC install.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ else:
 # return to regular viewing
 setup(
     name='BamM',
-    version='1.3.2',
+    version='1.3.3',
     author='Michael Imelfort',
     author_email='mike@mikeimelfort.com',
     packages=['bamm'],
