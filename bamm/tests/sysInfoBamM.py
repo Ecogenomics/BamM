0c53acc466d8df609646d3117a1da3a820910c23
minillinim <mike@mikeimelfort.com>
2015-02-12 16:52:28 +1000

    for helping the peeps debug

---
+++ bamm/tests/sysInfoBamM.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+
+    try:
+        import numpy
+        print "Numpy version: %s" % numpy.__version__
+        print "Location: %s" % numpy.__file__
+    except ImportError:
+        print "Error importing numpy"
+
+    import platform
+    import sys
+    print platform.system(), platform.release()
+    print(sys.version)
+    print(sys.path)
