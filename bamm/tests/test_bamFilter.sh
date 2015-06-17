d4f05d24594a18cfdcb1d0f90d2109630533281a
Tim Lamberton <t.lamberton@uq.edu.au>
2015-06-17 13:36:06 +1000

    Added second test bam and script mode to generate test data.

--- bamm/tests/test_bamFilter.sh
+++ bamm/tests/test_bamFilter.sh
@@ -27,75 +27,118 @@
 ###############################################################################
 
 data_dir=~/git/BamM/bamm/tests/filter_test_data
-pre_bam=$data_dir/pre.bam
-output_dir=$data_dir/output
 bamm_exe=~/git/BamM/bin/bamm
 
+### arguments
+make=
+f=
+if [ "$1" == "make" ]
+then
+  bam_id=$2
+  make=1
+elif [ "$1" == "fail" ]
+then
+  bam_id=$2
+  f='f'
+else
+  bam_id=$1
+fi
+if [ ! "$bam_id" ]
+then echo "ERROR: Missing test .bam index" >&2; exit 1;
+fi
+
 ### Helpers ###
-OUTPUT_FILE=$output_dir/$(basename $pre_bam .bam)_filtered.bam
+output_dir=$data_dir/$bam_id/output
+OUTPUT_FILE=$output_dir/${bam_id}_filtered.bam
 FAILED=
+v=
 
 run_test() {
-  CURRENT_TEST="$bamm_exe filter -b $pre_bam -o $output_dir $@"
+  CURRENT_TEST="$bamm_exe filter -b $data_dir/$bam_id$f.bam -o $output_dir $@"
+  if [ $v ]
+  then echo "RUNNING: $CURRENT_TEST" >&2
+  fi
   $CURRENT_TEST
-  assert_equal_length
-  rm $OUTPUT_FILE
+  if [ $make ]
+  then
+    mv $OUTPUT_FILE $TEST_FILE
+  else
+    assert_equal_length && pass || fail
+    rm $OUTPUT_FILE
+  fi
 }
 
 assert_equal_length() {
-  size1=samtools view $OUTPUT_FILE |wc -l
-  size2=samtools view $TEST_FILE |wc -l
-  if [ $size1 != $size2 ]; then fail_test; fi
+  size1=$(samtools view $OUTPUT_FILE |wc -l)
+  size2=$(samtools view $TEST_FILE |wc -l)
+  if [ $size1 != $size2 ]
+  then return 1
+  else return 0
+  fi
 }
 
+pass() {
+  if [ $v ]
+  then echo "PASSED: $CURRENT_TEST" >&2
+  fi
+}
 
-fail_test() {
+fail() {
   echo "FAILED: $CURRENT_TEST" >&2
   FAILED=1;
 }
 
 tests_done() {
-  if [ $FAILED ]
+  if [ $make ]
   then
-    echo "There were failing test.">&2
+    echo "Finished generating data for test set $bam_id." >&2
+    exit 0
+  elif [ $FAILED ]
+  then
+    echo "There were failing tests for set $bam_id.">&2
     exit 1
   else
-    echo "All tests passed." >&2
+    echo "All tests passed for set $bam_id." >&2
     exit 0
   fi
 }
 
+test_file() {
+  TEST_FILE=$data_dir/$bam_id/${bam_id}_$1.bam
+}
+
 ### Test 1 ###
-TEST_FILE=$data_dir/none.bam
+test_file none
 run_test --use_secondary --use_supplementary --percentage_aln 0 --percentage_id 0
 
 ### Test 2 ###
-TEST_FILE=$data_dir/aln_only_0.9.bam
+test_file aln_only_0.9
 run_test --use_secondary --use_supplementary --percentage_aln 0.9
 
 ### Test 3 ###
-TEST_FILE=$data_dir/aln_only_1.01.bam
+test_file aln_only_1.01
 run_test --use_secondary --use_supplementary --percentage_aln 1.01
 
 ### Test 4 ###
-TEST_FILE=$data_dir/id_only_0.9.bam
+test_file id_only_0.9
 run_test --use_secondary --use_supplementary --percentage_id 0.9
 
 ### Test 5 ###
-TEST_FILE=$data_dir/id_only_1.01.bam
+test_file id_only_1.01
 run_test --use_secondary --use_supplementary --percentage_id 1.01
 
 ### Test 6 ###
-TEST_FILE=$data_dir/no_second_only.bam
+test_file no_secondary_only
 run_test --use_supplementary
 
 ### Test 7 ###
-TEST_FILE=$data_dir/no_supp_only.bam
+test_file no_supp_only
 run_test --use_secondary
 
 ### Test 8 ###
-TEST_FILE=$data_dir/all_conds.bam
+test_file all_conds
 run_test --percentage_aln 0.9 --percentage_id 0.9
 
 ### Clean up
 rmdir $output_dir
+tests_done
