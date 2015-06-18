5fe27a2750c2d668d181d3be3f890baa22adff69
Tim Lamberton <t.lamberton@uq.edu.au>
2015-06-18 16:36:56 +1000

    runBamFilter cleans up when finished.

--- c/runBamFilter.c
+++ c/runBamFilter.c
@@ -9,8 +9,10 @@
 int main( int argc, char *argv[] ) {
     if ( argc != 2 ) {
         printf( "USAGE: %s BAM_FILE\n", argv[0]);
+        return 1;
     }
     else {
         filterReads(argv[1], "out.bam", 0, 0, 1000, 0.9, 0.9, 0, 0);
+        return remove("out.bam");
     }
 }
